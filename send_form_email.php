a0114dd49d007305998e978bcb8cf828d84ef2cc
reubengaunt <reubengaunt@gmail.com>
2017-08-20 12:40:07 +0100

    baseUpload

---
+++ send_form_email.php
@@ -0,0 +1,87 @@
+<?php
+
+   header( 'Location: http://www.karenbowenweddingmakeup.com/wedding-makeup-fife-scotland-thanks.html' ) ;
+
+?>
+
+<?php
+if(isset($_POST['email'])) {
+     
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "k-bowen@hotmail.co.uk";
+    $email_subject = "Karen Bowen Wedding Makeup";
+     
+     
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+     
+    // validation expected data exists
+    if(!isset($_POST['full_name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['telephone']) ||
+        !isset($_POST['wedding_no']) ||
+        !isset($_POST['wedding_location']) ||
+        !isset($_POST['wedding_date']) ||
+        !isset($_POST['comments'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+     
+    $full_name = $_POST['full_name']; // required
+    $email_from = $_POST['email']; // required
+    $telephone = $_POST['telephone']; // not required
+    $wedding_no = $_POST['wedding_no']; // required
+    $wedding_location = $_POST['wedding_location']; // required
+    $wedding_date = $_POST['wedding_date']; // required
+    $comments = $_POST['comments']; // required
+     
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+    $string_exp = "/^[A-Za-z .'-]+$/";
+  if(!preg_match($string_exp,$full_name)) {
+    $error_message .= 'The Full Name you entered does not appear to be valid.<br />';
+  }
+    $string_exp = "/^[0-9]+$/";
+  if(!preg_match($string_exp,$wedding_no)) {
+    $error_message .= 'The Number of Makeups Required you entered does not appear to be valid.<br />';
+  }
+
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+    $email_message = "Form details below.\n\n";
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+     
+    $email_message .= "Name: ".clean_string($full_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+    $email_message .= "Number of Makeups Required: ".clean_string($wedding_no)."\n";
+    $email_message .= "Location: ".clean_string($wedding_location)."\n";
+    $email_message .= "Date: ".clean_string($wedding_date)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+     
+     
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+?>
+ 
+ 
+<?php
+}
+?>
+
