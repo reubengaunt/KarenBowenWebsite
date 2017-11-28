a1684fe87b18b56b48195dad5475b84e65d6a99e
reubengaunt <reubengaunt@gmail.com>
2017-11-28 23:51:27 +0000

    howManyDamnChanges

--- send_form_email.php
+++ send_form_email.php
@@ -76,7 +76,7 @@ if(isset($_POST['email'])) {
     $email_message .= "Comments: ".clean_string($comments)."\n";
  
 // create email headers
-$headers = 'From: '.$email_from."\r\n".
+$headers = 'From: info@karenbowenweddingmakeup.com'."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);  
